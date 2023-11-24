ba42d4950d23257b7822d277d9f19ff1c011a968
Michał Zwierzyński <michal.zwierzynski16@gmail.com>
2023-11-24 21:29:41 +0100

    Add Loading component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { COLORS } from './ui/utils';
 import Container from './ui/Container';
@@ -6,14 +6,24 @@ import Text from './ui/Text';
 import Input from './ui/Input';
 import Select from './ui/Select';
 import Button from './ui/Button';
+import LoadingOverlay from './ui/Loading';
 
 function App(): JSX.Element {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <Container>
       <StatusBar backgroundColor={COLORS.gray} barStyle={'light-content'} />
       <Text icon="faExclamationCircle">
         Warning! This is very early version of app! Might be updated in future
       </Text>
+      <Text>As I said this application may be not useful.</Text>
       <Input
         name={'Login'}
         placeholder={'Login'}
@@ -33,6 +43,7 @@ function App(): JSX.Element {
         }}
         title={'Accept'}
       />
+      <LoadingOverlay isVisible={isLoading} />
     </Container>
   );
 }
---
+++ ui/Loading.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Modal, ActivityIndicator, StyleSheet } from 'react-native';
+import { COLORS } from './utils';
+
+interface ILoadingOverlay {
+  isVisible: boolean;
+}
+
+export default function LoadingOverlay({ isVisible }: ILoadingOverlay) {
+  return (
+    <Modal transparent animationType="none" visible={isVisible}>
+      <View style={styles.overlay}>
+        <View style={styles.spinnerContainer}>
+          <ActivityIndicator size={80} color={COLORS.blue} />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinnerContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
