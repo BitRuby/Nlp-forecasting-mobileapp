a1b6049f9ced5428bf639a2dfb6ef8afcda00582
Michał Zwierzyński <michal.zwierzynski16@gmail.com>
2023-11-26 20:31:57 +0100

    Add List component

---
+++ ui/List.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, FlatList, StyleSheet, TouchableHighlight } from 'react-native';
+import { COLORS, FONT_SIZE } from './utils';
+
+type ItemProps = { id: number; data: any };
+
+const Item = ({ data }: ItemProps) =>
+  Object.keys(data).map((key: string) => (
+    <Text key={data[key]} style={styles.itemText}>
+      {data[key]}
+    </Text>
+  ));
+
+interface IList {
+  data: Array<{ id: number; data: any }>;
+}
+
+export default function List({ data }: IList) {
+  const renderItem = ({ item }: any) => (
+    <TouchableHighlight
+      onPress={() => {}}
+      underlayColor={COLORS.lightGray}
+      style={styles.itemCointainer}>
+      <Item id={item.id} data={item.data} />
+    </TouchableHighlight>
+  );
+
+  return (
+    <FlatList
+      renderItem={renderItem}
+      data={data}
+      keyExtractor={(item: ItemProps) => item.id.toString()}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  itemCointainer: {
+    borderColor: COLORS.white,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 8,
+    marginVertical: 5,
+  },
+  itemText: {
+    color: COLORS.white,
+    fontFamily: 'Poppins-Light',
+    fontSize: FONT_SIZE,
+  },
+});
